396e4959ff33d52a2cb1f1d199eaa511e684db38
Krishna Kapoor <88818689+Krshnna@users.noreply.github.com>
2022-09-04 20:03:47 +0530

    Sqrt(x)
    https://leetcode.com/problems/sqrtx/

---
+++ Sqrt(x).cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long int i = 1;
+        while(i * i <= x) {
+            i++;
+        }
+        return i - 1;
+    }
+};
+
+
+// the complexity of this code is sqrt(n)
